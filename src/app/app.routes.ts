2e28f428cf4d48fc7e728c4ff2abfce9cc64cf6a
Darius Tiganas <adarius9.1999@gmail.com>
2025-06-20 11:13:15 +0200

    organizacion de rutas con sus componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,12 @@ import AddComponent from './pages/add/add';
 import ProfileComponent from './pages/profile/profile';
 
 
+/* 
+Estos son los paths que usara la aplicacion, basicamente tenemos la pantalla
+de login que se mantiene hasta que el usuario se loggea o registra, despues
+se mantiene 'main' como la pagina por defecto, o como el path por defecto (**)
+*/
+
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 
