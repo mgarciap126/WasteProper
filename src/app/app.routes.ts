55f25a99d81e7b6ee25ad9ee71392be7fdcc6eba
Darius Tiganas <adarius9.1999@gmail.com>
2025-06-20 11:12:41 +0200

    Organizacion de las rutas  de todos los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,24 @@
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 
-export const routes: Routes = [];
+import LoginComponent from './pages/login/login';
+import RegisterComponent from './pages/register/register';
+import LogoutComponent from './pages/logout/logout';
+import MainComponent from './pages/main/main';
+import AddComponent from './pages/add/add';
+import ProfileComponent from './pages/profile/profile';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'logout', component: LogoutComponent },
+    
+    { path: 'main', component: MainComponent },
+    { path: 'add', component: AddComponent },
+    { path: 'profile', component: ProfileComponent },
+
+    { path: '**', redirectTo: 'login' }
+];
